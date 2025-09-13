21c9a2482cc862ec422c199c80be06b68c496e7b
zhangzexin <zzx-nuc@qq.com>
2025-09-13 21:38:26 +0800

    producer: replace OnnxScoringModel with HzScoringModel in ContentAggregatorProducer

--- src/main/java/org/hzai/ai/aiproducer/ContentAggregatorProducer.java
+++ src/main/java/org/hzai/ai/aiproducer/ContentAggregatorProducer.java
@@ -1,23 +1,23 @@
 package org.hzai.ai.aiproducer;
 
-import dev.langchain4j.model.scoring.onnx.OnnxScoringModel;
 import dev.langchain4j.rag.content.aggregator.ContentAggregator;
 import dev.langchain4j.rag.content.aggregator.ReRankingContentAggregator;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+
+import org.hzai.ai.scoringmodel.HzScoringModel;
 
 @ApplicationScoped
 public class ContentAggregatorProducer {
+    @Inject
+    HzScoringModel hzScoringModel;
     @Produces
     @ApplicationScoped
     public ContentAggregator contentAggregator() {
         // 评分模型
-        String pathToModel = "/home/zzx/IdeaProjects/bge-reranker-base/onnx/model.onnx";
-        String pathToTokenizer = "/home/zzx/IdeaProjects/bge-reranker-base/tokenizer.json";
-        OnnxScoringModel scoringModel = new OnnxScoringModel(pathToModel, pathToTokenizer);
-
         return ReRankingContentAggregator.builder()
-                .scoringModel(scoringModel)
+                .scoringModel(hzScoringModel)
                 .minScore(0.8)
                 .build();
     }
