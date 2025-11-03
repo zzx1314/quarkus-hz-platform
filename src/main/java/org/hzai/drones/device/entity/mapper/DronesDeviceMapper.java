6934c69c35fdc06722058ceacad6f8c4632e5183
zhangzexin <zzx-nuc@qq.com>
2025-11-03 18:20:42 +0800

    feat(device): add null value property mapping strategy to device mapper

--- src/main/java/org/hzai/drones/device/entity/mapper/DronesDeviceMapper.java
+++ src/main/java/org/hzai/drones/device/entity/mapper/DronesDeviceMapper.java
@@ -2,9 +2,11 @@ package org.hzai.drones.device.entity.mapper;
 
 import org.hzai.drones.device.entity.DronesDevice;
 import org.hzai.drones.device.entity.dto.DronesDeviceDto;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "cdi" , unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -12,8 +14,10 @@ public interface DronesDeviceMapper {
     DronesDevice toEntity(DronesDeviceDto dto);
 
     @Mapping(target = "id", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEntityFromDto(DronesDeviceDto dto, @MappingTarget DronesDevice entity);
 
     @Mapping(target = "id", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEntity(DronesDevice dto, @MappingTarget DronesDevice entity);
 }
